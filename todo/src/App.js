97983d5646eae9cb8f64e0c18cc02401801ff9a4
Pamela Paprasz <papraszpamela@gmail.com>
2018-04-07 15:20:58 +0200

    add, data model to the app and sign it ti the internal component state

--- todo/src/App.js
+++ todo/src/App.js
@@ -2,7 +2,43 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// 1. ADD DATA MODEL TO THE APP
+var todos = [
+  {
+    todoTitle: 'My first todo',
+    todoResponsible: 'Pami',
+    todoDescription: 'My first todo description',
+    todoPriority: 'low'
+  },
+  {
+    todoTitle: 'My second todo',
+    todoResponsible: 'Pami',
+    todoDescription: 'My second todo description',
+    todoPriority: 'medium'
+  },
+  {
+    todoTitle: 'My third todo',
+    todoResponsible: 'Pami',
+    todoDescription: 'My third todo description',
+    todoPriority: 'heigh'
+  }
+];
+
+// 2. SIGN DATA MODEL (make it avaliable) TO THE INTERNAL COMPONENT STATE
+// every component has an internal component state which contains the data which is used by the component and the state is an object which can be modified 
+
 class App extends Component {
+
+  //first we SET THE INITIAL STATE IN THE CONSTRUCTOR in order to make the todos array available in the internal component state of our component
+  constructor(props) {
+    // CALLING SUPER YOU MAKE SURE THAT PROPS PARAMETER IS PASSED TO THE PARENT CONSTRUCTOR TOO
+    super(props);
+
+    this.state = {
+      todos
+    };
+  }
+
   render() {
     return (
       <div className="App">
