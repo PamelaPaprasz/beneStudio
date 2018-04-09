9ec623718c638cc0cdb2d33c408013ba115a9331
Pamela Paprasz <papraszpamela@gmail.com>
2018-04-09 04:02:02 +0200

    make title and responsible fields editable too

--- todo/src/App.js
+++ todo/src/App.js
@@ -38,9 +38,13 @@ class App extends Component {
     })
   }
 
-  handleSaveTodo(oldTask, newTask) {
-    const foundTodo = _.find(this.state.todos, todo => todo.todoDescription === oldTask);
-    foundTodo.todoDescription = newTask;
+  handleSaveTodo(oldTitle, newTitle, oldResponsible, newResponsible, oldDesc, newDesc) {
+    const foundTitle = _.find(this.state.todos, todo => todo.todoTitle === oldTitle);
+    const foundResponsible = _.find(this.state.todos, todo => todo.todoResponsible === oldResponsible);
+    const foundDesc = _.find(this.state.todos, todo => todo.todoDescription === oldDesc);
+    foundTitle.todoTitle = newTitle;
+    foundResponsible.todoResponsible = newResponsible;
+    foundDesc.todoDescription = newDesc;
     this.setState({ todos: this.state.todos });
   }
 
@@ -51,15 +55,36 @@ class App extends Component {
   handleEditTodo(index) {
     if(this.state.todos[index].isEditing) { 
       return(
-        <textarea name="todoDescription"
-                  row="3"
-                  id="inputTodoDesc"
-                  defaultValue={this.state.todos[index].todoDescription}
-                  ref="editInput" />
+        <div>
+          <input name="todoTitle"
+                  type="text"
+                  autoComplete="off"
+                  id="inputTodoTitle"
+                  defaultValue={this.state.todos[index].todoTitle}
+                  ref="editInputTitle">
+          </input>
+          <input name="todoResponsible"
+                  type="text"
+                  autoComplete="off"
+                  id="inputTodoResponsible"
+                  defaultValue={this.state.todos[index].todoResponsible}
+                  ref="editInputResponsible">
+          </input>
+          <textarea name="todoDescription"
+                    row="3"
+                    id="inputTodoDesc"
+                    defaultValue={this.state.todos[index].todoDescription}
+                    ref="editInputDesc" />
+        </div>
       )
     }
     return(
-      <p>{this.state.todos[index].todoDescription}</p> 
+      <div>
+        <h4>{this.state.todos[index].todoTitle}<small><span>{this.state.todos[index].todoPriority}</span></small></h4>
+                
+        <p>{this.state.todos[index].todoResponsible}</p> 
+        <p>{this.state.todos[index].todoDescription}</p> 
+      </div>
     );
   }
 
@@ -91,9 +116,13 @@ class App extends Component {
   }
 
   onSaveClick(index) {
-    const oldTask = this.state.todos[index].todoDescription;
-    const newTask = this.refs.editInput.value;
-    this.handleSaveTodo(oldTask, newTask);
+    const oldTitle = this.state.todos[index].todoTitle;
+    const newTitle = this.refs.editInputTitle.value;
+    const oldResponsible = this.state.todos[index].todoResponsible;
+    const newResponsible = this.refs.editInputResponsible.value;
+    const oldDesc = this.state.todos[index].todoDescription;
+    const newDesc = this.refs.editInputDesc.value;
+    this.handleSaveTodo(oldTitle, newTitle, oldResponsible, newResponsible, oldDesc, newDesc);
     this.state.todos[index].isEditing = false;
     this.setState({ todos: this.state.todos })
   }
@@ -109,9 +138,7 @@ class App extends Component {
         <ul>
           {this.state.todos.map((todo, index) =>
             <li key={index}>
-              <h4>{todo.todoTitle}<small><span>{todo.todoPriority}</span></small></h4>
-              
-              <p>{todo.todoResponsible}</p> 
+
               {this.handleEditTodo(index)}   
 
               <button onClick={this.handleRemoveTodo.bind(this, index)}>Delete</button>
